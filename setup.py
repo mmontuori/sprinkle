353e2477c960f2bb8c982acf9356efc271351dc2
mmontuori <michael.montuori@gmail.com>
2018-10-16 23:46:56 -0400

    new setup.py

--- setup.py
+++ setup.py
@@ -9,22 +9,39 @@ __license__ = "GPLv3"
 __version__ = "0.1"
 __revision__ = "1"
 
+import os
 from setuptools import setup
 from setuptools import find_packages
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+this = os.path.dirname(os.path.realpath(__file__))
+
+def read(name):
+    with open(os.path.join(this, name)) as f:
+        return f.read()
+
 setup(
     name='sprinkle',
     version='0.1.1',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    #packages=['sprinkle'],
+    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=['sprinkle'],
+    install_requires=read('requirements.txt'),
     url='https://gitlab.com/mmontuori/sprinkle',
-    license='',
+    license='GPLv3',
+    include_package_data=True,
     author='mmontuori',
     author_email='michael.montuori@gmail.com',
     description='Sprinkle and backup files in the cloud across multiple storage',
     long_description=long_description,
-    long_description_content_type="text/markdown"
+    keywords='sprinkle cloud backup restore',
+    zip_safe=True,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English'
+    ],
+    scripts=['sprinkle.py']
 )
